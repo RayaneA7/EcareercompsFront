eaf50fe347cb5d773a1c435079b6b9a4ff37043e
RayaneA7 <ka_kebir@esi.dz>
2022-05-23 23:03:16 +0100

    commit 1

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+function downloadObjectAsJson (exportObj, exportName) {
+  var dataStr =
+    'data:text/json;charset=utf-8,' +
+    encodeURIComponent(JSON.stringify(exportObj))
+  var downloadAnchorNode = document.createElement('a')
+  downloadAnchorNode.setAttribute('href', dataStr)
+  downloadAnchorNode.setAttribute('download', exportName + '.json')
+  document.body.appendChild(downloadAnchorNode) // required for firefox
+  downloadAnchorNode.click()
+  downloadAnchorNode.remove()
+}
+document.querySelector('button').addEventListener('click', async e => {
+  document.body.style.cursor = 'wait'
+  document.querySelector('button').style.cursor = 'wait'
+  console.log('gello')
+  const url = 'http://localhost:8080'
+  var headers = {}
+
+  fetch(url, {
+    method: 'GET',
+    mode: 'cors',
+    headers: headers
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.error)
+      }
+      return response.json()
+    })
+    .then(data => {
+      downloadObjectAsJson(data, 'competences')
+      console.log(data)
+      document.body.style.cursor = 'pointer'
+      document.querySelector('button').style.cursor = ''
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+})
